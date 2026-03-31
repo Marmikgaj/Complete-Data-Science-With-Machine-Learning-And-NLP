7f8beab9f6f2049d46bcbc5532c7f6dce2bc751d
marmikgaj <marmikgaj005@gmail.com>
2026-04-01 01:59:09 +0530

    Model Trainer

--- 24- End to End ML Proj/src/components/data_ingestion.py
+++ 24- End to End ML Proj/src/components/data_ingestion.py
@@ -1,3 +1,4 @@
+from src.components.model_trainer import ModelTrainer
 import os
 import sys
 from src.exception import CustomException
@@ -54,3 +55,6 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_training(train_arr, test_arr))
--- 24- End to End ML Proj/src/utils.py
+++ 24- End to End ML Proj/src/utils.py
@@ -4,6 +4,8 @@ import dill
 
 from src.exception import CustomException
 
+from sklearn.metrics import r2_score
+
 def save_objects(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -12,6 +14,29 @@ def save_objects(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            
+            # Train the models
+            model.fit(X_train, y_train)
+            
+            # Evaluate the models
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
